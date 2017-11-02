b1191d0bf320346a5bd4b0fc358e7f4cdbb2dfe9
L Shaf <shafry@mimicreative.net>
2017-11-02 20:45:35 +0700

    fix datatable and custom styling

--- src/assets/plugins/DataTableAsset.php
+++ src/assets/plugins/DataTableAsset.php
@@ -11,12 +11,8 @@ use akupeduli\material\assets\core\PluginAsset;
 class DataTableAsset extends PluginAsset
 {
     public $pluginName = "datatables";
-    public $css = [
-        "jquery.dataTables.min.css"
-    ];
     public $js = [
-        "jquery.dataTables.min.js",
-        "dataTables.bootstrap.js"
+        "jquery.dataTables.min.js"
     ];
     public $depends = [
         "yii\\web\\JqueryAsset",
