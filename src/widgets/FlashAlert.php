003572fc3db3a063abf196d203bf3cc206507c5a
L Shaf <shafry@mimicreative.net>
2017-11-03 16:09:44 +0700

    add widgets display alerts based on session flash

---
+++ src/widgets/FlashAlert.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace akupeduli\material\widgets;
+
+use yii\bootstrap\Widget;
+use yii\bootstrap\Alert;
+use yii\helpers\Html;
+
+/**
+ * Alert widget renders a message from session flash for AdminLTE alerts. All flash messages are displayed
+ * in the sequence they were assigned using setFlash. You can set message as following:
+ *
+ * ```php
+ * \Yii::$app->getSession()->setFlash('error', '<b>Alert!</b> Danger alert preview. This alert is dismissable.');
+ * ```
+ *
+ * Multiple messages could be set as follows:
+ *
+ * ```php
+ * \Yii::$app->getSession()->setFlash('error', ['Error 1', 'Error 2']);
+ * ```
+ *
+ * @author Evgeniy Tkachenko <et.coder@gmail.com>
+ */
+class FlashAlert extends Widget
+{
+    /**
+     * @var array the alert types configuration for the flash messages.
+     * This array is setup as $key => $value, where:
+     * - $key is the name of the session flash variable
+     * - $value is the array:
+     *       - class of alert type (i.e. danger, success, info, warning)
+     *       - icon for alert AdminLTE
+     */
+    public $alertTypes = [
+        'error' => [
+            'class' => 'alert-danger',
+            'headClass' => 'text-danger',
+            'icon' => '<i class="fa fa-ban"></i>',
+        ],
+        'danger' => [
+            'class' => 'alert-danger',
+            'headClass' => 'text-danger',
+            'icon' => '<i class="fa fa-ban"></i>',
+        ],
+        'success' => [
+            'class' => 'alert-success',
+            'headClass' => 'text-success',
+            'icon' => '<i class="fa fa-check"></i>',
+        ],
+        'info' => [
+            'class' => 'alert-info',
+            'headClass' => 'text-info',
+            'icon' => '<i class="fa fa-info"></i>',
+        ],
+        'warning' => [
+            'class' => 'alert-warning',
+            'headClass' => 'text-warning',
+            'icon' => '<i class="fa fa-warning"></i>',
+        ],
+    ];
+    
+    /**
+     * @var array the options for rendering the close button tag.
+     */
+    public $closeButton = [];
+    
+    
+    /**
+     * @var boolean whether to removed flash messages during AJAX requests
+     */
+    public $isAjaxRemoveFlash = true;
+    
+    /**
+     * Initializes the widget.
+     * This method will register the bootstrap asset bundle. If you override this method,
+     * make sure you call the parent implementation first.
+     */
+    public function init()
+    {
+        parent::init();
+    
+        $session = \Yii::$app->getSession();
+        $flashes = $session->getAllFlashes();
+        
+        foreach ($flashes as $type => $data) {
+            if (isset($this->alertTypes[$type])) {
+                $data = (array) $data;
+                foreach ($data as $message) {
+                    
+                    $alertData = $this->alertTypes[$type];
+                    $headTitle = $alertData['icon'] . " " . ucwords($type);
+                    $headTitle = Html::tag("h3", $headTitle, [ "class" => $alertData["headClass"] ]);
+    
+                    $options = $this->options;
+                    $options['id'] = $this->getId() . '-' . $type;
+                    Html::addCssClass($options, $alertData['class']);
+                    
+                    echo Alert::widget([
+                        'body' => $headTitle . $message,
+                        'closeButton' => $this->closeButton,
+                        'options' => $options,
+                    ]);
+                }
+                if ($this->isAjaxRemoveFlash && !\Yii::$app->request->isAjax) {
+                    $session->removeFlash($type);
+                }
+            }
+        }
+    }
+}
