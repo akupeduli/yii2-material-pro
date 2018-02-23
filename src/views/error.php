6c8986019ab40ff355618c368181bbeda9af2f10
L Shaf <shafry@mimicreative.net>
2018-02-23 10:15:03 +0700

    fix error home url

--- src/views/error.php
+++ src/views/error.php
@@ -1,5 +1,6 @@
 <?php
 use akupeduli\material\assets\core\ImageAsset;
+use yii\helpers\Url;
 
 /** @var $this \yii\web\View */
 /** @var $exception \yii\web\HttpException */
@@ -12,7 +13,11 @@ $this->title = "{$exception->statusCode} - $message";
             <h1><?= $exception->statusCode ?></h1>
             <h3 class="text-uppercase"><?= $message ?></h3>
             <p class="text-muted m-t-30 m-b-30">WE ARE TRYING OUR BEST TO SOLVE THIS</p>
-            <a href="/" class="btn btn-info btn-rounded waves-effect waves-light m-b-40">Back to home</a> </div>
+            <a href="<?= Url::to(Yii::$app->homeUrl) ?>"
+               class="btn btn-info btn-rounded waves-effect waves-light m-b-40">
+                Back to home
+            </a>
+        </div>
         <footer class="footer text-center"><?= Yii::$app->params["material"]["footer"] ?></footer>
     </div>
 </section>
